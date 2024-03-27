d173d318840d34d423f4d7f987e6e9331c71196a
Hunterosmun <Hunter@Osmun.net>
2024-03-27 09:23:22 -0600

    Switch game to ECS (mostly)

---
+++ OLD/app.js
@@ -0,0 +1,67 @@
+import * as PIXI from 'pixi.js'
+import { createGameState } from './game-state.js'
+
+const game = createGameState()
+
+const app = new PIXI.Application()
+await app.init({
+  background: '#1099bb',
+  resizeTo: window,
+})
+
+document.body.appendChild(app.canvas)
+
+const { pos, border, groundItems } = game.loop(0)
+
+const middleX = app.screen.width / 2
+const middleY = app.screen.height / 2
+
+const bunnyURL = 'https://pixijs.com/assets/bunny.png'
+const bunnyTexture = await PIXI.Assets.load(bunnyURL)
+const bunny = PIXI.Sprite.from(bunnyTexture)
+bunny.anchor.set(0.5, 0.5)
+bunny.x = middleX
+bunny.y = middleY
+
+const pizzaTexture = await PIXI.Assets.load('/assets/pizza.png')
+
+const background = new PIXI.Container()
+
+Object.values(groundItems).forEach((item) => {
+  if (item.type === 'pizza') {
+    const pizza = new PIXI.Sprite(pizzaTexture)
+    pizza.anchor.set(0.5)
+    pizza.x = 0 - border.x + item.x
+    pizza.y = 0 - border.y + item.y
+    background.addChild(pizza)
+    item.ref = pizza
+  }
+})
+
+const borderGraphics = new PIXI.Graphics()
+  .rect(0, 0, border.width, border.height)
+  .stroke({ width: 1, color: '0x000000' })
+
+background.addChild(borderGraphics)
+
+app.stage.addChild(bunny)
+app.stage.addChild(background)
+
+// Listen for animate update
+app.ticker.add((delta) => {
+  const { pos, spin } = game.loop(delta.deltaTime)
+  const middleX = app.screen.width / 2
+  const middleY = app.screen.height / 2
+
+  bunny.x = middleX
+  bunny.y = middleY
+
+  background.x = middleX + (border.x - pos.x)
+  background.y = middleY + (border.y - pos.y)
+
+  if (spin) bunny.rotation += delta.deltaTime
+
+  Object.values(groundItems).forEach((item) => {
+    if (item.isHeld) item.ref.removeFromParent()
+  })
+})
--- src/app.js
+++ src/app.js
@@ -1,67 +1,186 @@
 import * as PIXI from 'pixi.js'
-import { createGameState } from './game-state.js'
+import * as ecs from './ecs.js'
+import v from './validate.js'
 
-const game = createGameState()
+// 1: Create components
+const positionComponent = ecs.createComponent(
+  v.object({
+    x: v.number(),
+    y: v.number(),
+  })
+)
 
+const velocityComponent = ecs.createComponent(
+  v.object({
+    x: v.number(),
+    y: v.number(),
+  })
+)
+
+const playerControlledComponent = ecs.createComponent(v.undefined())
+
+const graphicsComponent = ecs.createComponent(
+  v.object({
+    pixiObject: v.or(v.instanceOf(PIXI.Graphics), v.instanceOf(PIXI.Sprite)),
+  })
+)
+
+// 2: Create systems
+const graphicsSystem = ecs.createSystem(
+  v.object({
+    container: v.instanceOf(PIXI.Container),
+  }),
+  { entities: [positionComponent, graphicsComponent] },
+  ({ container }, delta, { entities }) => {
+    for (const entity of entities) {
+      const { x, y } = ecs.getComponent(entity, positionComponent)
+      const { pixiObject } = ecs.getComponent(entity, graphicsComponent)
+      pixiObject.x = x
+      pixiObject.y = y
+      if (ecs.isAdded(entities, entity)) container.addChild(pixiObject)
+      if (ecs.isRemoved(entities, entity)) pixiObject.removeFromParent()
+    }
+  }
+)
+
+const DIAGONAL_RADIANS = Math.sin(Math.PI / 4)
+const SPEED = 100
+const RUN_MODIFIER = 2
+const KEY_BINDINGS = {
+  MOVE_UP: 'KeyW',
+  MOVE_DOWN: 'KeyS',
+  MOVE_LEFT: 'KeyA',
+  MOVE_RIGHT: 'KeyD',
+  RUN: 'ShiftLeft',
+  SPIN: 'Space',
+  INTERACT: 'KeyE',
+}
+
+const keyboardInputSystem = ecs.createSystem(
+  v.object({
+    up: v.boolean(),
+    down: v.boolean(),
+    left: v.boolean(),
+    right: v.boolean(),
+    running: v.boolean(),
+    window: v.instanceOf(Window),
+  }),
+  { entities: [velocityComponent, playerControlledComponent] },
+  ({ up, down, left, right, running }, delta, { entities }) => {
+    for (const entity of entities) {
+      const entityVelocity = ecs.getComponent(entity, velocityComponent)
+
+      let velocity = SPEED
+      if (running) velocity *= RUN_MODIFIER
+      const goinDiagonal = up !== down && left !== right
+      if (goinDiagonal) velocity * -DIAGONAL_RADIANS
+
+      entityVelocity.x = 0
+      entityVelocity.y = 0
+      if (up) entityVelocity.y = -velocity
+      if (down) entityVelocity.y = velocity
+      if (left) entityVelocity.x = -velocity
+      if (right) entityVelocity.x = velocity
+    }
+  },
+  (state) => {
+    function handleKeyDown(e) {
+      if (e.code === KEY_BINDINGS.MOVE_UP) state.up = true
+      if (e.code === KEY_BINDINGS.MOVE_DOWN) state.down = true
+      if (e.code === KEY_BINDINGS.MOVE_LEFT) state.left = true
+      if (e.code === KEY_BINDINGS.MOVE_RIGHT) state.right = true
+      if (e.code === KEY_BINDINGS.RUN) state.running = true
+    }
+    function handleKeyUp(e) {
+      if (e.code === KEY_BINDINGS.MOVE_UP) state.up = false
+      if (e.code === KEY_BINDINGS.MOVE_DOWN) state.down = false
+      if (e.code === KEY_BINDINGS.MOVE_LEFT) state.left = false
+      if (e.code === KEY_BINDINGS.MOVE_RIGHT) state.right = false
+      if (e.code === KEY_BINDINGS.RUN) state.running = false
+    }
+    state.window.addEventListener('keydown', handleKeyDown)
+    state.window.addEventListener('keyup', handleKeyUp)
+    return () => {
+      state.window.removeEventListener('keydown', handleKeyDown)
+      state.window.removeEventListener('keyup', handleKeyUp)
+    }
+  }
+)
+
+const physicsSystem = ecs.createSystem(
+  v.null(),
+  { entities: [positionComponent, velocityComponent] },
+  (state, delta, { entities }) => {
+    for (const entity of entities) {
+      const position = ecs.getComponent(entity, positionComponent)
+      const { x: x, y: y } = ecs.getComponent(entity, velocityComponent)
+      position.x += x * delta
+      position.y += y * delta
+    }
+  }
+)
+
+const backgroundSystem = ecs.createSystem(
+  v.object({
+    background: v.instanceOf(PIXI.Container),
+    screen: v.instanceOf(PIXI.Rectangle),
+  }),
+  { players: [positionComponent, playerControlledComponent] },
+  ({ background, screen }, delta, { players }) => {
+    const middleX = screen.width / 2
+    const middleY = screen.height / 2
+
+    for (const player of players) {
+      const { x, y } = ecs.getComponent(player, positionComponent)
+      background.x = middleX - x
+      background.y = middleY - y
+    }
+  }
+)
+
+// 3: Create world
 const app = new PIXI.Application()
 await app.init({
   background: '#1099bb',
   resizeTo: window,
 })
 
+const background = new PIXI.Container()
+app.stage.addChild(background)
 document.body.appendChild(app.canvas)
 
-const { pos, border, groundItems } = game.loop(0)
+const borderGraphics = new PIXI.Graphics()
+  .rect(0, 0, 500, 500)
+  .stroke({ width: 1, color: '#000000' })
+background.addChild(borderGraphics)
 
-const middleX = app.screen.width / 2
-const middleY = app.screen.height / 2
+const world = ecs
+  .createWorld()
+  .registerSystem(keyboardInputSystem, {
+    down: false,
+    up: false,
+    left: false,
+    right: false,
+    running: false,
+    window,
+  })
+  .registerSystem(physicsSystem, null)
+  .registerSystem(backgroundSystem, { background, screen: app.screen })
+  .registerSystem(graphicsSystem, { container: background })
+
+// 4: Create entities
 
 const bunnyURL = 'https://pixijs.com/assets/bunny.png'
 const bunnyTexture = await PIXI.Assets.load(bunnyURL)
 const bunny = PIXI.Sprite.from(bunnyTexture)
-bunny.anchor.set(0.5, 0.5)
-bunny.x = middleX
-bunny.y = middleY
 
-const pizzaTexture = await PIXI.Assets.load('/assets/pizza.png')
+const player = world.createEntity()
+ecs.addComponent(player, positionComponent, { x: 250, y: 250 })
+ecs.addComponent(player, velocityComponent, { x: 0, y: 0 })
+ecs.addComponent(player, playerControlledComponent, undefined)
+ecs.addComponent(player, graphicsComponent, { pixiObject: bunny })
 
-const background = new PIXI.Container()
-
-Object.values(groundItems).forEach((item) => {
-  if (item.type === 'pizza') {
-    const pizza = new PIXI.Sprite(pizzaTexture)
-    pizza.anchor.set(0.5)
-    pizza.x = 0 - border.x + item.x
-    pizza.y = 0 - border.y + item.y
-    background.addChild(pizza)
-    item.ref = pizza
-  }
-})
-
-const borderGraphics = new PIXI.Graphics()
-  .rect(0, 0, border.width, border.height)
-  .stroke({ width: 1, color: '0x000000' })
-
-background.addChild(borderGraphics)
-
-app.stage.addChild(bunny)
-app.stage.addChild(background)
-
-// Listen for animate update
-app.ticker.add((delta) => {
-  const { pos, spin } = game.loop(delta.deltaTime)
-  const middleX = app.screen.width / 2
-  const middleY = app.screen.height / 2
-
-  bunny.x = middleX
-  bunny.y = middleY
-
-  background.x = middleX + (border.x - pos.x)
-  background.y = middleY + (border.y - pos.y)
-
-  if (spin) bunny.rotation += delta.deltaTime
-
-  Object.values(groundItems).forEach((item) => {
-    if (item.isHeld) item.ref.removeFromParent()
-  })
+// 5: Start the game loop
+app.ticker.add((ticker) => {
+  world.execute(ticker.deltaMS / 1000)
 })
